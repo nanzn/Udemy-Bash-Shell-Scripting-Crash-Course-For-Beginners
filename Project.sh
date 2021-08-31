c59aca27224dd8bba2dce7f0fa26048e39c3a5c7
Zhou Nan <nanzn.zhou@gmail.com>
2021-08-31 08:04:18 +0800

    Updated Project

--- Project.sh
+++ Project.sh
@@ -1,5 +1,27 @@
 #! /bin/bash
 
+# Function to copy file with search word to /tmp/out
+copy_file() {
+    local search="$1"
+    local filepath="$2"
+    # Differentiate between files and directories
+    if [ -f "$filepath" ]; then # File
+        # Find specific word in a file
+        result=$(grep -in $search "$filepath")
+        # Copy if result is found
+        if [ -z "$result" ]; then
+            echo "NOT FOUND: $filepath"
+        else
+            echo "FOUND: $filepath"
+            filename=$(basename $filepath)
+            cp "$filepath" /tmp/out/"$filename"
+            echo "" >> /tmp/out/"$filename"
+            echo "******************" >> /tmp/out/"$filename"
+            echo "$result" >> /tmp/out/"$filename"
+        fi
+    fi
+}
+
 # Remove and make new directory to store output
 rm -r /tmp/out
 mkdir /tmp/out
@@ -7,26 +29,20 @@ mkdir /tmp/out
 # Set word to find
 word="Apple" 
 
-# Create a loop on all items in a folder
-for i in *;
+# Loop through all directories in a folder
+for dir in $(find . -type d);
 do
-    # Differentiate between files and directories
-    if [ -f "$i" ]; then # File
-        # Find specific word in a file
-        result=$(grep -in $word "$i")
-        # Copy if result is found
-        if [ -z "$result" ]; then
-            echo "Not Found in $i"
-        else
-            echo "Found in $i"
-            cp "$i" /tmp/out/
-            echo "" >> /tmp/out/"$i"
-            echo "******************" >> /tmp/out/"$i"
-            echo "$result" >> /tmp/out/"$i"
-        fi
-    else # Directory
-        echo "$i is not a file"
+    # Ensure it does not check output folder
+    if [ $dir != "/tmp/out" ] ; then
+        echo "=== Checking in: $dir ==="
+        #Loop through all items in directory
+        for i in $dir/* ;
+        do
+            copy_file $word $i
+        done
     fi
 done
 
-ls -l /tmp/out
+# Check
+echo "=== Results ==="
+ls /tmp/out
